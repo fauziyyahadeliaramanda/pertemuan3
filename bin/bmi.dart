43631491387f66e8545d5abff76d53b78dccfcac
Fauziyyah Adelia Ramanda <framandaadelia@gmail.com>
2025-09-17 15:16:06 +0700

    feat: input-validation

---
+++ bin/bmi.dart
@@ -0,0 +1,12 @@
+// main.dart
+void main() {
+  List<Map<String, dynamic>> riwayat = [];
+
+  // Simulasi beberapa input
+  hitungBMI(170, 65, riwayat);
+  hitungBMI(165, 75, riwayat);
+  hitungBMI(180, 85, riwayat);
+
+  // Tampilkan riwayat menggunakan perulangan
+  tampilkanRiwayat(riwayat);
+}
